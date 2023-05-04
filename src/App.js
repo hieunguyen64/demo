dcf20591b551499c4e4a043e478f2515d0229223
Hieu Nguyen <hieu.nd@fahasa.com.vn>
2023-05-04 09:31:21 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
 
 function App() {
+  const [customerName, setCustomerName] = useState("");
+
+  const handleTextChange = (event) => {
+    setCustomerName(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    // Add search functionality here
+  };
+
+  const handleSaveClick = () => {
+    // Add save functionality here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h2 style={{ fontSize: "1.5rem", marginBottom: "1rem" }}>Nhập tên khách hàng</h2>
+      <TextField
+        variant="outlined"
+        placeholder="Nhập tên khách hàng"
+        style={{ marginBottom: "1rem", width: "100%", maxWidth: "20rem" }}
+        value={customerName}
+        onChange={handleTextChange}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ marginBottom: "1rem", minWidth: "8rem" }}
+        onClick={handleSearchClick}
+      >
+        Tìm kiếm
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        style={{ marginBottom: "1rem", minWidth: "8rem" }}
+        onClick={handleSaveClick}
+      >
+        Lưu tên khách hàng
+      </Button>
+    </form>
   );
 }
 
